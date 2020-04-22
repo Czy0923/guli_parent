b694f0d7db92699edd45035c74267e03aa857e39
churui <churuigongzuo@163.com>
2020-04-22 10:21:47 +0800

    项目第二次提交

---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -0,0 +1,67 @@
+package com.atguigu.eduservice.controller;
+
+
+import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.mapper.EduTeacherMapper;
+import com.atguigu.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 讲师 前端控制器
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-04-21
+ */
+@Api(description = "讲师管理")
+@RestController
+@RequestMapping("/eduservice/edu-teacher")
+public class EduTeacherController {
+
+    @Autowired
+    private EduTeacherService teacherService;
+
+    @ApiOperation(value = "所有讲师列表")
+    @GetMapping("findAll")
+    public R findAllTeacher() {
+        List<EduTeacher> list = teacherService.list(null);
+        return R.ok().data("items", list);
+    }
+
+    @ApiOperation(value = "逻辑删除讲师")
+    @DeleteMapping("{id}")
+    public R removeTeacher(@ApiParam(name = "id", value = "讲师ID", required = true) @PathVariable String id) {
+        boolean flag = teacherService.removeById(id);
+        if (flag) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
+    }
+
+    //3 分页查询讲师的方法
+    //current 当前页
+    //limit 每页记录数
+    @ApiOperation(value = "分页查询讲师数据")
+    @GetMapping("pageTeacher/{current}/{limit}")
+    public R pageListTeacher(@PathVariable long current,
+                             @PathVariable long limit) {
+
+        Page<EduTeacher> pageTeacher = new Page<>(current, limit);
+        teacherService.page(pageTeacher, null);
+        long total = pageTeacher.getTotal();
+        List<EduTeacher> records = pageTeacher.getRecords();
+        return R.ok().data("total", total).data("rows", records);
+    }
+}
+
---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/service/impl/EduTeacherServiceImpl.java
@@ -0,0 +1,24 @@
+package com.atguigu.eduservice.service.impl;
+
+import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.mapper.EduTeacherMapper;
+import com.atguigu.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 讲师 服务实现类
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-04-21
+ */
+@Service
+public class EduTeacherServiceImpl extends ServiceImpl<EduTeacherMapper, EduTeacher> implements EduTeacherService {
+
+
+}
